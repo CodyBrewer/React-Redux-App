7b00ede7a2a58e873f7743f8fc6be64b95f99f8b
Cody Brewer <brewer.cody.m@gmail.com>
2020-02-09 02:01:40 -0700

    added race reducer to rootReducer

--- client/src/app/rootReducer.js
+++ client/src/app/rootReducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 // import reducers from "../store/reducers/";
 import classReducer from "../store/reducers/classReducer";
 import characterReducer from "../store/reducers/characterReducer";
+import raceReducer from "../store/reducers/raceReducer";
 
 export default combineReducers({
   // add & describe reducers here
   classes: classReducer,
-  character: characterReducer
+  character: characterReducer,
+  races: raceReducer
 });
