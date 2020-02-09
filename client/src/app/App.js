602795cd27086afd7a6f80617fac4b9886369da5
Cody Brewer <brewer.cody.m@gmail.com>
2020-02-09 02:02:57 -0700

    change fetch function on component did mount to get races and classes

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -3,20 +3,44 @@ import { Switch, Route } from "react-router-dom";
 import Routes from "./Routes";
 import CharacterSheet from "../components/Character/CharacterSheet";
 import Navigation from "../components/Navigation/Navigation";
+import CharactarClasses from "../components/Classes/Classes";
 
-const App = () => {
-  return (
-    <>
-      <Navigation />
-      <Switch>
-        {Routes.map(route => (
-          <Route exact path={route.path} key={route.path}>
-            <route.component />
-          </Route>
-        ))}
-      </Switch>
-    </>
-  );
+import { fetchClasses, fetchRaces } from "../store/actions";
+import { connect } from "react-redux";
+
+class App extends React.Component {
+  componentDidMount() {
+    this.props.firstFetch();
+  }
+
+  render() {
+    return (
+      <>
+        <Navigation />
+        <Switch>
+          {Routes.map(route => (
+            <Route exact path={route.path} key={route.path}>
+              <route.component />
+            </Route>
+          ))}
+        </Switch>
+      </>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {};
 };
 
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+    firstFetch: () => {
+      console.log(`hi`);
+      dispatch(fetchClasses());
+      dispatch(fetchRaces());
+    }
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
